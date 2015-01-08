84852d19e24f0dae84999924da5c1994840f91d9
root <root@dev.(none)>
2015-01-08 03:51:46 +0000

    Added some new models.

---
+++ backend/api/controllers/RegionController.js
@@ -0,0 +1,11 @@
+/**
+ * RegionController
+ *
+ * @description :: Server-side logic for managing regions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
