a098146301e258b51016ca4b947da2a4cca8ba10
elwebmaster <elwebmaster@gmail.com>
2015-02-12 04:26:48 -0500

    Lightswitch

---
+++ Realtor/Realtor.HTMLClient/Screens/AddEditPhoto.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditPhoto.PhotoFile_render = function (element, contentItem) {
+    // Write code here.
+    createImageUploader(element, contentItem, "max-width: 300px; max-height: 300px");
+};
